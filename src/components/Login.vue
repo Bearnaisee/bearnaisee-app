55343636ca47d43fbc317e42a7b25bc004600558
Mads Hougesen <madshougesen@gmail.com>
2022-02-16 13:24:54 +0100

    feat: basic login & signup components

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form @submit="login">
+      <p>Email</p>
+      <input type="email" v-model="email" />
+
+      <p>Password</p>
+      <input type="password" v-model="password" />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Login',
+
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    async login() {
+      if (this.email?.length && this.password?.length) {
+        const HOST = 'http://localhost:5000';
+        const URL = `${HOST}/user/login`;
+
+        const result = await axios.post(URL, {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        });
+
+        console.log('createUser RESULT', result);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit="createUser">
+      <p>Username</p>
+      <input type="text" v-model="username" />
+
+      <p>Email</p>
+      <input type="email" v-model="email" />
+
+      <p>Password</p>
+      <input type="password" v-model="password" />
+
+      <button type="submit">Signup</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Signup',
+
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    };
+  },
+
+  methods: {
+    async createUser() {
+      if (this.username?.length && this.email?.length && this.password?.length) {
+        const HOST = 'http://localhost:5000';
+        const URL = `${HOST}/user/signup`;
+
+        const result = await axios.post(URL, {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        });
+
+        console.log('createUser RESULT', result);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
