2c2a6b41765449190dd8e1e1b06b0e484a4590d7
André Hørslev Dijkhuizen <andredijkhuizen@gmail.com>
2022-02-28 15:11:27 +0100

    recipe start

--- src/views/user/Recipe.vue
+++ src/views/user/Recipe.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    <h1>{{ $route.params.slug }}</h1>
 
+    <div class="recipe-img"></div>
+    <div class="recipe-wrapper">
+      <h1>{{ $route.params.slug }}</h1>
+    
     <h2>By {{ $route.params.username }}</h2>
+    </div>
+    
+    <RecipeCard />
   </div>
 </template>
 
 <script>
+import {defineAsyncComponent} from 'vue';
+
 export default {
   name: 'Recipe',
+
+  components: {
+      RecipeCard: defineAsyncComponent(() => import('@/components/RecipeCard.vue'))
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .recipe-img{
+    height: 50px;
+    width: auto;
+    background-color: red;
+  }
+  .recipe-wrapper{
+    background-color: orange;
+    border-radius: 50% 50% 0 0;
+  }
+</style>
