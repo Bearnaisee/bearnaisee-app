19b7b2a5627b68d4122a6212b3ec403e1dbb567f
Filip Pedersen <filip.pedersen98@gmail.com>
2022-03-02 09:04:36 +0100

    display of steps

--- src/views/user/Recipe.vue
+++ src/views/user/Recipe.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-    <h1>{{ $route.params.slug }}</h1>
+  <div style="display: inline-block">
+    <div>
+      <h1>{{ $route.params.slug }}</h1>
 
-    <h2>By {{ $route.params.username }}</h2>
+      <h2>By {{ $route.params.username }}</h2>
+    </div>
+
+    <ul>
+      <li v-for="step in steps" :key="step">
+        <div>
+          {{ step.title }}
+        </div>
+
+        <div>
+          {{ step.desc }}
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Recipe',
+
+  props: {},
+
+  data() {
+    return {
+      steps: [
+        { title: 'Step 1', desc: 'cook your pasta al dente' },
+        { title: 'Step 2', desc: 'brown your meats' },
+        { title: 'Step 3', desc: 'drizzle with cheese' },
+      ],
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li {
+  text-decoration: none;
+}
+ul {
+  list-style-type: none;
+}
+</style>
