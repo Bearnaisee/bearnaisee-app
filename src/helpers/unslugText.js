e3f5688fbeebdc220a23eec65915f064d49f81b5
Mads Hougesen <madshougesen@gmail.com>
2022-05-18 16:17:49 +0200

    fix: return text if null

--- src/helpers/unslugText.js
+++ src/helpers/unslugText.js
@@ -3,6 +3,9 @@
  * @returns {string}
  */
 export default function unslugText(text) {
+  if (!text?.length) return text;
+
   const decodedURI = decodeURI(text)?.replaceAll('-', ' ');
+
   return `${decodedURI[0].toUpperCase()}${decodedURI.slice(1, decodedURI?.length)}`;
 }
