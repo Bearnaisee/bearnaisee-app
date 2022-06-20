a938f9de168bc5a4c917dc5cc0e2d1bb2df9372c
Dijkhuizen <andr440n@edu.ucl.dk>
2022-06-20 16:03:15 +0200

    Comments

---
+++ src/components/AddComment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form">
+        <form>
+            <div class="form__group">
+                    <label>Skriv en kommentar</label>
+                    <textarea v-model="newComment" required cols="30" rows="10" placeholder="Skriv her..."></textarea>
+                    <button>Send</button>
+            </div>
+        </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            newComment: '',
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
